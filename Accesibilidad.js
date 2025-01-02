7a96cde5ea49a871ff9db8c1ecd36d9ab531eb6c
BrayanAdair <71566782+BrayanAdair@users.noreply.github.com>
2025-01-02 12:35:11 -0600

    Update Accesibilidad.js 0.2

--- Accesibilidad.js
+++ Accesibilidad.js
@@ -1,18 +1,19 @@
-!function(){let e=document.getElementById("bz-accesibilidad-widget");if(!e){console.error('No se encontr\xf3 el elemento con id "bz-accesibilidad-widget"');return}e.innerHTML=`
+(function(){const widgetContainer=document.getElementById('bz-accesibilidad-widget');if(!widgetContainer){console.error('No se encontró el elemento con id "bz-accesibilidad-widget"');return}
+widgetContainer.innerHTML=`
     <a class="btn-Accesibilidad" id="Accesibilidad">
       <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_logoAcc.png" style="width: 100%; height: 100%;">
     </a>
 
-    <div id="bz-accesibilidad" class="modal">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h2 class="bz-titulo">Men\xfa Accesibilidad </h2> &nbsp; <small> (CTRL + M)</small>
+    <div id="bz-accesibilidad" class="bz-modal">
+      <div class="bz-modal-content">
+        <div class="bz-modal-header">
+          <h2 class="bz-titulo">Menú Accesibilidad </h2> &nbsp; <b class="bz-subtitulo"> (CTRL + M)</b>
           <span class="close" onclick="closeModal()">&times;</span>
         </div>
-        <div class="modal-body">
+        <div class="bz-modal-body">
           <div>
             <a href="#" class="textos-voz no-redimensionar">
-              <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_9.png" alt="Leer textos con voz"><p class="texto-dinamico">Leer p\xe1gina</p>
+              <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_9.png" alt="Leer textos con voz"><p class="texto-dinamico">Leer página</p>
             </a>
           </div>
           <div>
@@ -23,14 +24,14 @@
           </div>
           <div>
             <a href="#" class="aumentar-tamano no-redimensionar">
-              <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_1.png" alt="Icono Subir Tama\xf1o"><p></p>
-              Subir Tama\xf1o+
+              <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_1.png" alt="Icono Subir Tamaño"><p></p>
+              Subir Tamaño+
             </a>
           </div>
           <div>
             <a href="#" class="disminuir-tamano no-redimensionar">
-              <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_2.png" alt="Icono Bajar Tama\xf1o"><p></p>
-              Bajar Tama\xf1o-
+              <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_2.png" alt="Icono Bajar Tamaño"><p></p>
+              Bajar Tamaño-
             </a>
           </div>
           <div>
@@ -41,8 +42,8 @@
           </div>
           <div>
             <a href="#" class="guia-lectura no-redimensionar">
-              <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_4.png" alt="Gu\xeda Lectura"><p></p>
-              Gu\xeda de lectura
+              <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_4.png" alt="Guía Lectura"><p></p>
+              Guía de lectura
             </a>
           </div>
           <div>
@@ -65,12 +66,12 @@
           </div>
           <div>
             <a href="#" class="cambiar-posicion no-redimensionar">
-              <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_8.png" alt="Posicion del Men\xfa"><p></p>
-              Posici\xf3n
+              <img src="https://accesibilidad.bzsoftwares.com/thumbnails/tn_8.png" alt="Posicion del Menú"><p></p>
+              Posición
             </a>
           </div>
         </div>
-        <div class="modal-footer">
+        <div class="bz-modal-footer">
           <a href="https://accesibilidad.bzsoftwares.com/">accesibilidad.bzsoftwares.com</a>
         </div>
       </div>
@@ -79,4 +80,9 @@
     <div class="cursor">
       <div class="hl"></div>
     </div>
-  `}();const domElements="a:not(.no-redimensionar),p:not(.no-redimensionar),h1:not(.no-redimensionar),h2:not(.no-redimensionar),h3:not(.no-redimensionar),h4:not(.no-redimensionar),h5:not(.no-redimensionar),h6:not(.no-redimensionar)",stepnum=3,minFontSize=10,ajustarTamanoFuente=(e,a)=>{document.querySelectorAll(e).forEach(e=>{let i=getComputedStyle(e),t=parseFloat(i.fontSize.replace("px",""))+a;t>=10&&(e.style.fontSize=`${t}px`)})},aumentarTamano=()=>{ajustarTamanoFuente(domElements,3)},disminuirTamano=()=>{ajustarTamanoFuente(domElements,-3)},aumentarBtn=document.querySelector(".aumentar-tamano"),disminuirBtn=document.querySelector(".disminuir-tamano"),modoNocturnoBtn=document.querySelector(".cambiar-contraste"),guiaBtn=document.querySelector(".guia-lectura"),resaltarBtn=document.querySelector(".resaltar-objeto"),cursorBtn=document.querySelector(".cambiar-cursor");aumentarBtn.onclick=e=>{e.preventDefault(),aumentarTamano()},disminuirBtn.onclick=e=>{e.preventDefault(),disminuirTamano()},modoNocturnoBtn.onclick=e=>{e.preventDefault(),document.body.classList.toggle("modo-nocturno")},guiaBtn.onclick=e=>{e.preventDefault(),document.querySelector(".cursor").classList.toggle("activo")},resaltarBtn.onclick=e=>{e.preventDefault();document.querySelectorAll(domElements).forEach(e=>{e.classList.toggle("highlight")})},cursorBtn.onclick=e=>{e.preventDefault(),document.body.classList.toggle("new-cursor")};const cursorHL=document.querySelector(".hl");document.addEventListener("mousemove",e=>{cursorHL.setAttribute("style",`top: ${e.clientY}px;`)});const tamanosIniciales=new Map,guardarTamanosIniciales=e=>{document.querySelectorAll(e).forEach(e=>{let a=getComputedStyle(e),i=parseFloat(a.fontSize.replace("px",""));tamanosIniciales.set(e,i)})},restaurarTamanos=()=>{tamanosIniciales.forEach((e,a)=>{a.style.fontSize=`${e}px`})},reiniciarConfiguraciones=()=>{restaurarTamanos(),document.body.classList.remove("modo-nocturno"),document.body.classList.remove("modo-dislexia"),document.body.classList.remove("new-cursor"),stopReading(),document.querySelector(".cursor").classList.remove("activo");document.querySelectorAll(domElements).forEach(e=>{e.classList.remove("highlight")})};guardarTamanosIniciales(domElements);const reiniciarBtn=document.querySelector(".reiniciar-configuracion");reiniciarBtn.onclick=e=>{e.preventDefault(),reiniciarConfiguraciones()};const cambiarPosicionBtn=document.querySelector(".cambiar-posicion");let posicionDerecha=!0;const cambiarPosicionModal=()=>{let e=document.querySelector(".modal-content");posicionDerecha?(e.style.right="auto",e.style.left="15px"):(e.style.left="auto",e.style.right="15px"),posicionDerecha=!posicionDerecha};cambiarPosicionBtn.onclick=e=>{e.preventDefault(),cambiarPosicionModal()};const dislexiaBtn=document.querySelector(".dislexia");dislexiaBtn.onclick=e=>{e.preventDefault(),document.body.classList.toggle("modo-dislexia")};const textosVozBtn=document.querySelector(".textos-voz"),textosVozTexto=textosVozBtn.querySelector(".texto-dinamico");let isReading=!1;textosVozBtn.onclick=e=>{e.preventDefault(),isReading?stopReading():startReading()};const startReading=()=>{let e=window.getSelection().toString();if(!e){alert("Por favor, selecciona el texto que deseas escuchar.");return}let a=new SpeechSynthesisUtterance(e);a.lang="es-MX",a.rate=1,a.pitch=1,a.volume=1;let i=window.speechSynthesis.getVoices(),t=i.find(e=>"es-ES"===e.lang&&e.name.includes("Google"));t&&(a.voice=t),a.onstart=()=>{isReading=!0,textosVozTexto.textContent="Detener lectura"},a.onend=()=>{isReading=!1,textosVozTexto.textContent="Leer con voz"},window.speechSynthesis.speak(a)},stopReading=()=>{window.speechSynthesis.cancel(),isReading=!1,textosVozTexto.textContent="Leer con voz"};function closeModal(){let e=document.getElementById("bz-accesibilidad");e.style.display="none"}function toggleModal(e){"block"===e.style.display?e.style.display="none":e.style.display="block"}document.getElementById("Accesibilidad").addEventListener("click",function(){let e=document.getElementById("bz-accesibilidad");toggleModal(e)}),window.addEventListener("click",function(e){let a=document.getElementById("bz-accesibilidad");e.target===a&&closeModal()}),window.addEventListener("keydown",function(e){if(e.ctrlKey&&"m"===e.key.toLowerCase()){let a=document.getElementById("bz-accesibilidad");toggleModal(a)}});
+  `})();const domElements='a:not(.no-redimensionar),p:not(.no-redimensionar),h1:not(.no-redimensionar),h2:not(.no-redimensionar),h3:not(.no-redimensionar),h4:not(.no-redimensionar),h5:not(.no-redimensionar),h6:not(.no-redimensionar)';const stepnum=3;const minFontSize=10;const ajustarTamanoFuente=(el,step)=>{let elements=document.querySelectorAll(el);elements.forEach(el=>{let style=getComputedStyle(el);let fontNum=parseFloat(style.fontSize.replace('px',''));let newFontSize=fontNum+step;if(newFontSize>=minFontSize){el.style.fontSize=`${newFontSize}px`}})};const aumentarTamano=()=>{ajustarTamanoFuente(domElements,stepnum)};const disminuirTamano=()=>{ajustarTamanoFuente(domElements,-stepnum)};const aumentarBtn=document.querySelector('.aumentar-tamano');const disminuirBtn=document.querySelector('.disminuir-tamano');const modoNocturnoBtn=document.querySelector('.cambiar-contraste');const guiaBtn=document.querySelector('.guia-lectura');const resaltarBtn=document.querySelector('.resaltar-objeto');const cursorBtn=document.querySelector('.cambiar-cursor');aumentarBtn.onclick=e=>{e.preventDefault();aumentarTamano()};disminuirBtn.onclick=e=>{e.preventDefault();disminuirTamano()};modoNocturnoBtn.onclick=e=>{e.preventDefault();document.body.classList.toggle('modo-nocturno')};guiaBtn.onclick=e=>{e.preventDefault();document.querySelector('.cursor').classList.toggle('activo')};resaltarBtn.onclick=e=>{e.preventDefault();let elements=document.querySelectorAll(domElements);elements.forEach(el=>{el.classList.toggle('highlight')})};cursorBtn.onclick=e=>{e.preventDefault();document.body.classList.toggle('new-cursor')};const cursorHL=document.querySelector('.hl');document.addEventListener('mousemove',e=>{cursorHL.setAttribute('style',`top: ${e.clientY}px;`)});const tamanosIniciales=new Map();const guardarTamanosIniciales=(el)=>{let elements=document.querySelectorAll(el);elements.forEach(el=>{let style=getComputedStyle(el);let fontNum=parseFloat(style.fontSize.replace('px',''));tamanosIniciales.set(el,fontNum)})};const restaurarTamanos=()=>{tamanosIniciales.forEach((size,el)=>{el.style.fontSize=`${size}px`})};const reiniciarConfiguraciones=()=>{restaurarTamanos();document.body.classList.remove('modo-nocturno');document.body.classList.remove('modo-dislexia');document.body.classList.remove('new-cursor');stopReading();document.querySelector('.cursor').classList.remove('activo');let elements=document.querySelectorAll(domElements);elements.forEach(el=>{el.classList.remove('highlight')})};guardarTamanosIniciales(domElements);const reiniciarBtn=document.querySelector('.reiniciar-configuracion');reiniciarBtn.onclick=e=>{e.preventDefault();reiniciarConfiguraciones()};const cambiarPosicionBtn=document.querySelector('.cambiar-posicion');let posicionDerecha=!0;const cambiarPosicionModal=()=>{const modal=document.querySelector('.bz-modal-content');if(posicionDerecha){modal.style.right='auto';modal.style.left='15px'}else{modal.style.left='auto';modal.style.right='15px'}
+posicionDerecha=!posicionDerecha};cambiarPosicionBtn.onclick=e=>{e.preventDefault();cambiarPosicionModal()};const dislexiaBtn=document.querySelector('.dislexia');dislexiaBtn.onclick=e=>{e.preventDefault();document.body.classList.toggle('modo-dislexia')};const textosVozBtn=document.querySelector('.textos-voz');const textosVozTexto=textosVozBtn.querySelector('.texto-dinamico');let isReading=!1;textosVozBtn.onclick=e=>{e.preventDefault();if(isReading){stopReading()}else{startReading()}};const startReading=()=>{const selectedText=window.getSelection().toString();if(!selectedText){alert("Por favor, selecciona el texto que deseas escuchar.");return}
+const utterance=new SpeechSynthesisUtterance(selectedText);utterance.lang='es-MX';utterance.rate=1;utterance.pitch=1;utterance.volume=1;const voices=window.speechSynthesis.getVoices();const preferredVoice=voices.find(voice=>voice.lang==='es-ES'&&voice.name.includes('Google'));if(preferredVoice){utterance.voice=preferredVoice}
+utterance.onstart=()=>{isReading=!0;textosVozTexto.textContent='Detener lectura'};utterance.onend=()=>{isReading=!1;textosVozTexto.textContent='Leer con voz'};window.speechSynthesis.speak(utterance)};const stopReading=()=>{window.speechSynthesis.cancel();isReading=!1;textosVozTexto.textContent='Leer con voz'};document.getElementById('Accesibilidad').addEventListener('click',function(){const modal=document.getElementById('bz-accesibilidad');toggleModal(modal)});function closeModal(){const modal=document.getElementById('bz-accesibilidad');modal.style.display='none'}
+function toggleModal(modal){if(modal.style.display==='block'){modal.style.display='none'}else{modal.style.display='block'}}
+window.addEventListener('click',function(event){const modal=document.getElementById('bz-accesibilidad');if(event.target===modal){closeModal()}});window.addEventListener('keydown',function(event){if(event.ctrlKey&&event.key.toLowerCase()==='m'){const modal=document.getElementById('bz-accesibilidad');toggleModal(modal)}})
